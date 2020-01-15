bf9fe3e04f1112aef0deebb4b213a1f1d84df698
Sergey <anaxronik@gmail.com>
2020-01-15 08:22:58 +0500

    Отредактирован рид ми

--- sql.py
+++ sql.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 print('Версия SQLAlchemy =', sqlalchemy.__version__)
 
-engine = create_engine('sqlite:///DB.db')
+engine = create_engine('sqlite:///bases/DB.db')
 
 Base = declarative_base()
 
